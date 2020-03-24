134e4eb3d1c7ad947cb0d7c6d5c9bd2b7839bfb1
Lisbey Urrea <lisbey.urrea@sofka.com.co>
2020-03-24 18:11:23 -0500

    historial de movimientos almacenado

--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import Board from './components/Board'
+import Game from './components/Game'
 import './index.css';
-
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
 
 
 ReactDOM.render(
